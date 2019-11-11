fea4e3bc00d24c3a1451159cacfe25bf2c1507bc
Dex Maddison <dexmente@gmail.com>
2019-11-11 02:32:37 -0500

    update index

--- Porsche/html/index.php
+++ Porsche/html/index.php
@@ -1,18 +1,37 @@
 <?php 
 
+function dd ($var) {
+    // echo '<pre>';
+    die(var_dump($var));
+    // echo '</pre>';
+}
 
-function includeFileContent ($file_name) {
+function getHTML ($file_name) {
     ob_start();
     ob_implicit_flush(false);
     include ($file_name);
     return ob_get_clean();
 }
 
-$content = includeFileContent('main.htm');
 
-if (@$_SERVER[REQUEST_URI] == '/autos') $inner_content = 'autos';
-else $inner_content = 'home'; 
+$content = getHTML('main.htm');
 
-$inner_content = includeFileContent("{$inner_content}.htm");
+$paths=['categories'];
+$path = trim(@$_SERVER[PATH_INFO], '/');
+
+
+if (in_array($path, $paths)) $inner_content_path = $path;  
+else $inner_content_path = 'home';
+
+if (file_exists("{$inner_content_path}.php")) {
+    $inner_content = getHTML("{$path}.htm");
+    include ("{$inner_content_path}.php");
+
+}
+else $inner_content = getHTML("home.htm");;
+
 $content = str_replace('[[content]]', $inner_content, $content);
-echo str_replace('[[cdn_domain]]/css/porsche', '/css', $content);
+
+$content = str_replace('[[cdn_domain]]/css/porsche', '/css', $content);
+
+echo $content;
