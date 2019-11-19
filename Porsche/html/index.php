89679df6cce72fae4e43d3d38ef1d7e2a5d85251
Dex Maddison <dexmente@gmail.com>
2019-11-19 14:46:40 -0500

    routes and main

--- Porsche/html/index.php
+++ Porsche/html/index.php
@@ -6,42 +6,34 @@ function dd ($var) {
     // echo '</pre>';
 }
 
-function getHTML ($file_name) {
+function getHTML (string $file_name) {
     ob_start();
     ob_implicit_flush(false);
-    include ("views/$file_name.htm");
+    include ("views/{$file_name}.htm");
     return ob_get_clean();
 }
 
 
 $content = getHTML('main');
-
-$paths=[
-    'categories',
-    'cotizador',
-    'retoma',
-    'test-drive',
-    'nuestras-sedes',
-    'contactanos',
-    'autos'
-];
+$routes = include ('routes.php');
 $path = trim(@$_SERVER[PATH_INFO], '/');
 
-// if ($path == 'autos') $path = 'categories';
+if ($path == '') $path = 'home';
 
-if (in_array($path, $paths)) $inner_content_path = $path;  
-else $inner_content_path = 'home';
+if (in_array($path, $routes)) $inner_content_path = $path;  
+else $inner_content_path = 'blank';
+
 
 if (file_exists("{$inner_content_path}.php")) {
     $inner_content = getHTML($path);
     include ("{$inner_content_path}.php");
 }
-else if (file_exists($inner_content_path)) $inner_content = getHTML($inner_content_path); 
-else $inner_content = getHTML("home");;
+else if (file_exists("views/{$inner_content_path}.htm") || file_exists("views/{$inner_content_path}.htm") ) $inner_content = getHTML($inner_content_path); 
+else $inner_content = getHTML("blank");
+
 
 
 $content = str_replace('[[page_content]]', $inner_content, $content);
-
 $content = str_replace('[[cdn_domain]]/css/porsche', '/css', $content);
 
 echo $content;
