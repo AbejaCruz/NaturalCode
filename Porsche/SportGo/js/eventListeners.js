f4cebf47946eb247680a64ceb7324c5a33fc1f57
Dex Maddison <dexmente@gmail.com>
2020-01-02 17:07:46 -0500

    re make folders

--- Porsche/SportGo/js/eventListeners.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-let variants = [...document.getElementsByClassName("variant")];
-
-variants.forEach (function (variant) {
-    variant.addEventListener('click', function () {
-        let container = this.closest('div');
-    });
-});
